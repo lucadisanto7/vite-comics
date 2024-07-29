7a5a9d1016ae6ac2fdae1dd9d1daab7efd96a993
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-29 18:38:29 +0200

    [UPD] Added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,9 +5,63 @@ export default {
 </script>
 <template>
     <div>
+        <body>
+            <div class="bg-dc d-flex justify-content-center align-items-center">
+                <img src="../assets/dc-logo-bg.png" alt="DC Logo">
+            </div>
         
+            <div class="footer-bg">
+                <div class="container">
+                    <div class="row footer-links">
+                        <div class="col-md-2">
+                            <h5>DC COMICS</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
+                            <h5>SHOP</h5>
+                            <ul class="list-unstyled">
+                              <li><a href="#">Shop DC</a></li>
+                              <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-2">
+                            <h5>DC</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy policy (New)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-2">
+                            <h5>SITES</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
     </div>
 </template>
 <style>
-    
+
 </style>

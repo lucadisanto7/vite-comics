7a6198d6cb3b02d5e6881ed5a4f6a69a28e841c1
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-29 17:43:03 +0200

    [UPD] Added AppMain to app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   components:{
-    AppHeader
+    AppHeader,
+    AppMain
   }
 }
 </script>
 <template>
   <div>
     <AppHeader />
+    <AppMain />
   </div>
 </template>
 <style>

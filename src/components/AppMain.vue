8d0bb37847b43ffafe9d022b9ea8b2331ad8fd3c
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-29 17:52:39 +0200

    [UPD] Added icons html

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,17 +4,47 @@ export default {
 }
 </script>
 <template>
-    <main class="bg-dark text-white py-5">
+    <main>
+    <section class="bg-dark text-white py-5">
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1>--> Content goes here <--</h1>
+                    <h1>-> Content goes here <-</h1>
                 </div>
             </div>
         </div>
+    </section>
+    <section class="bg-blue text-white py-5">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col d-flex align-items-center">
+                    <img src="../assets/buy-comics-digital-comics.png" alt="Digital Comics" class="icon mb-2">
+                    <h5>Digital Comics</h5>
+                </div>
+                <div class="col d-flex align-items-center">
+                    <img src="../assets/buy-comics-merchandise.png" alt="DC Merchandise" class="icon mb-2">
+                    <h5>DC Merchandise</h5>
+                </div>
+                <div class="col d-flex align-items-center">
+                    <img src="../assets/buy-comics-subscriptions.png" alt="Subscription" class="icon mb-2">
+                    <h5>Subscription</h5>
+                </div>
+                <div class="col d-flex align-items-center">
+                    <img src="../assets/buy-comics-shop-locator.png" alt="Comic Shop Locator" class="icon-2 icon mb-2">
+                    <h5>Comic Shop Locator</h5>
+                </div>
+                <div class="col d-flex align-items-center">
+                    <img src="../assets/buy-dc-power-visa.svg" alt="DC Power Visa" class="icon mb-2">
+                    <h5>DC Power Visa</h5>
+                </div>
+            </div>
+        </div>
+    </section>
+    
     </main>
     
 </template>
 <style>
-    
+
+
 </style>
